49e40bcda94b67178916678c116e658f8ff1e76e
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-01-11 22:38:30 -0500

    completely remove firebase

--- ts/function.ts
+++
@@ -1,8 +0,0 @@
-function calculateDaysBetweenDates(startDate, endDate) {
-    const millisecondsPerDay = 86400 * 1000;
-    const startDateInMs = startDate.getTime();
-    const endDateInMs = endDate.getTime();
-    const differenceInMs = endDateInMs - startDateInMs;
-    const differenceInDays = Math.round(differenceInMs / millisecondsPerDay);
-    return differenceInDays;
-}
